2f24f09bc92d18d4723399ab4b9392294fcdf08e
Akhilesh <akhilesh0707@gmail.com>
2021-05-18 00:57:40 +0530

    Update UI on data change (State management)

--- app/src/main/java/com/aqube/compose/MainActivity.kt
+++ app/src/main/java/com/aqube/compose/MainActivity.kt
@@ -3,36 +3,55 @@ package com.aqube.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.aqube.compose.ui.theme.JetpackcomposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetpackcomposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
+            MainScreen()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MainScreen() {
+    val greetingListState = remember { mutableStateListOf<String>("John", "Chris", "Andrew") }
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.SpaceEvenly,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        GreetingList(greetingListState) {
+            greetingListState.add("Dona")
+        }
+    }
 }
+
+@Composable
+fun GreetingList(namesList: List<String>, buttonClick: () -> Unit) {
+    namesList.forEach { name ->
+        Text(text = name, style = MaterialTheme.typography.h4)
+    }
+    Button(onClick = { buttonClick.invoke() }) {
+        Text(text = "Add name")
+    }
+}
+
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    JetpackcomposeTheme {
-        Greeting("Android")
-    }
+    MainScreen()
 }
