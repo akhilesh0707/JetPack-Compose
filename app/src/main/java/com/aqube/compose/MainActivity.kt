2fd7598a743e12feb62c33ec9d505a81771317c6
Akhilesh <akhilesh0707@gmail.com>
2021-05-18 01:24:37 +0530

    Dynamic value from TextField

--- app/src/main/java/com/aqube/compose/MainActivity.kt
+++ app/src/main/java/com/aqube/compose/MainActivity.kt
@@ -9,11 +9,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
@@ -28,22 +31,35 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MainScreen() {
     val greetingListState = remember { mutableStateListOf<String>("John", "Chris", "Andrew") }
+    var newNameState = remember { mutableStateOf("") }
+
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.SpaceEvenly,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        GreetingList(greetingListState) {
-            greetingListState.add("Dona")
-        }
+        GreetingList(greetingListState,
+            { greetingListState.add(newNameState.value) },
+            newNameState.value,
+            { newNameState.value = it }
+        )
     }
 }
 
 @Composable
-fun GreetingList(namesList: List<String>, buttonClick: () -> Unit) {
+fun GreetingList(
+    namesList: List<String>,
+    buttonClick: () -> Unit,
+    textFieldValue: String,
+    textFieldChange: (name: String) -> Unit
+) {
     namesList.forEach { name ->
         Text(text = name, style = MaterialTheme.typography.h4)
     }
+
+
+    TextField(value = textFieldValue, onValueChange = textFieldChange)
+
     Button(onClick = { buttonClick.invoke() }) {
         Text(text = "Add name")
     }
