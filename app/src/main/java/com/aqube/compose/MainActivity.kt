cb2204d2cc6b937f8eaadc0ceab09fd35b610615
Akhilesh <akhilesh0707@gmail.com>
2021-05-22 23:23:00 +0530

    Extract main screen to another file and deleted static profile images

--- app/src/main/java/com/aqube/compose/MainActivity.kt
+++ app/src/main/java/com/aqube/compose/MainActivity.kt
@@ -3,26 +3,9 @@ package com.aqube.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Menu
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.aqube.compose.ui.theme.JetpackcomposeTheme
-import com.aqube.compose.ui.theme.lightGreen
-import com.google.accompanist.coil.rememberCoilPainter
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,93 +18,6 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun MainScreen(userProfiles: List<UserProfile> = users) {
-    Scaffold(topBar = { AppBar() }) {
-        Surface(modifier = Modifier.fillMaxSize()) {
-            LazyColumn {
-                items(userProfiles) { user ->
-                    ProfileCard(user)
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun AppBar() {
-    TopAppBar(
-        title = {
-            Text(text = "Compose Demo", color = Color.White)
-        },
-        navigationIcon = {
-            IconButton(onClick = { }) {
-                Icon(Icons.Filled.Menu, "")
-            }
-        }
-    )
-}
-
-@Composable
-fun ProfileCard(userProfile: UserProfile) {
-    Card(
-        modifier = Modifier
-            .padding(top = 16.dp, start = 16.dp, end = 16.dp)
-            .fillMaxWidth()
-            .wrapContentHeight(align = Alignment.Top),
-        elevation = 8.dp,
-        backgroundColor = Color.White
-    ) {
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.Start
-        ) {
-            ProfilePicture(userProfile)
-            ProfileContent(userProfile)
-        }
-    }
-}
-
-@Composable
-fun ProfilePicture(userProfile: UserProfile) {
-    Card(
-        shape = CircleShape,
-        border = BorderStroke(
-            width = 1.dp,
-            color = if (userProfile.status)
-                MaterialTheme.colors.lightGreen
-            else Color.Red
-        ),
-        modifier = Modifier.padding(16.dp),
-        elevation = 4.dp
-    ) {
-        Image(
-            painter = rememberCoilPainter(userProfile.drawable),
-            contentDescription = "",
-            modifier = Modifier.size(70.dp),
-            contentScale = ContentScale.Crop
-        )
-    }
-}
-
-@Composable
-fun ProfileContent(userProfile: UserProfile) {
-    Column(
-        modifier = Modifier
-            .padding(8.dp)
-            .fillMaxWidth()
-    ) {
-        Text(text = userProfile.name, style = MaterialTheme.typography.h6)
-        CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
-            Text(
-                text = if (userProfile.status) "Active now" else "Offline",
-                style = MaterialTheme.typography.body2
-            )
-        }
-    }
-}
-
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
@@ -129,3 +25,4 @@ fun DefaultPreview() {
         MainScreen()
     }
 }
+
---
+++ app/src/main/java/com/aqube/compose/MainScreen.kt
@@ -0,0 +1,107 @@
+package com.aqube.compose
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+import com.aqube.compose.ui.theme.lightGreen
+import com.google.accompanist.coil.rememberCoilPainter
+
+@Composable
+fun MainScreen(userProfiles: List<UserProfile> = users) {
+    Scaffold(topBar = { AppBar() }) {
+        Surface(modifier = Modifier.fillMaxSize()) {
+            LazyColumn {
+                items(userProfiles) { user ->
+                    ProfileCard(user)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun AppBar() {
+    TopAppBar(
+        title = {
+            Text(text = "Compose Demo", color = Color.White)
+        },
+        navigationIcon = {
+            IconButton(onClick = { }) {
+                Icon(Icons.Filled.Menu, "")
+            }
+        }
+    )
+}
+
+@Composable
+fun ProfileCard(userProfile: UserProfile) {
+    Card(
+        modifier = Modifier
+            .padding(top = 16.dp, start = 16.dp, end = 16.dp)
+            .fillMaxWidth()
+            .wrapContentHeight(align = Alignment.Top),
+        elevation = 8.dp,
+        backgroundColor = Color.White
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start
+        ) {
+            ProfilePicture(userProfile)
+            ProfileContent(userProfile)
+        }
+    }
+}
+
+@Composable
+fun ProfilePicture(userProfile: UserProfile) {
+    Card(
+        shape = CircleShape,
+        border = BorderStroke(
+            width = 1.dp,
+            color = if (userProfile.status)
+                MaterialTheme.colors.lightGreen
+            else Color.Red
+        ),
+        modifier = Modifier.padding(16.dp),
+        elevation = 4.dp
+    ) {
+        Image(
+            painter = rememberCoilPainter(userProfile.pictureUrl),
+            contentDescription = "",
+            modifier = Modifier.size(70.dp),
+            contentScale = ContentScale.Crop
+        )
+    }
+}
+
+@Composable
+fun ProfileContent(userProfile: UserProfile) {
+    Column(
+        modifier = Modifier
+            .padding(8.dp)
+            .fillMaxWidth()
+    ) {
+        Text(text = userProfile.name, style = MaterialTheme.typography.h6)
+        CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+            Text(
+                text = if (userProfile.status) "Active now" else "Offline",
+                style = MaterialTheme.typography.body2
+            )
+        }
+    }
+}
